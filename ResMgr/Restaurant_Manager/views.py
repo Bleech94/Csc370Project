7f15786700fe363b4a8abff737a0af4cde271cd1
Justin Richard <justinr@uvic.ca>
2015-11-23 18:29:22 -0800

    django in html?!??! weird

--- ResMgr/Restaurant_Manager/views.py
+++ ResMgr/Restaurant_Manager/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, render_to_response, RequestContext
 from django.http import HttpResponse
+
+from Restaurant_Manager.models import FoodItem
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'Restaurant_Manager/index.html')
-
+	dishes = FoodItem.objects.all()
+	return render(request, 'Restaurant_Manager/index.html', {'dishes': dishes})
+	
 def account(request):
     return render(request, 'Restaurant_Manager/account.html')
